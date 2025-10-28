deb555f0a7baa69fef699d623838acab685634e7
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-10-28 21:14:05 +0100

    chore: init

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from '~/components/ui/button/Button.vue';
+
+</script>
+
+<template>
+  <div>
+    <Button>holy moly ts a button</Button>
+  </div>
+</template>
