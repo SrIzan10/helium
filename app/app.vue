f6453bc32ee9fa21599f6b329f61bb3699b58e90
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-10-28 21:41:42 +0100

    chore: color setter

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
-<script setup lang="ts">
-import Button from '~/components/ui/button/Button.vue';
-
-</script>
-
 <template>
-  <div>
-    <Button>holy moly ts a button</Button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ThemeDropdown from '~/components/ui/ThemeDropdown.vue';
+
+</script>
+
+<template>
+  <div>
+    <ThemeDropdown />
+    <slot />
+  </div>
+</template>
