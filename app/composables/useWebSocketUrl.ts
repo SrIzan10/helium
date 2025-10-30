63d23210b8c9b9eae711aa0a5b39be4b2d36e93c
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2025-10-30 12:21:29 +0100

    chore: remove comments

--- app/composables/useWebSocketUrl.ts
+++ app/composables/useWebSocketUrl.ts
@@ -1,15 +1,9 @@
 export const useWebSocketUrl = () => {
-  // Detect environment based on hostname
-  // In production (helium.srizan.dev), use wss://
-  // In development (localhost), use ws://
   if (import.meta.client) {
     const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:'
     const host = window.location.host
     return `${protocol}//${host}/ws/signaling`
   }
-  
-  // Server-side: This shouldn't be used since WebSocket connections are client-only
-  // But we provide a sensible default for SSR hydration
   const config = useRuntimeConfig()
   const isDev = process.dev
   
